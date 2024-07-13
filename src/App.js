09173a4dcbb1daa9ea5a5de54e61e53ed7309cb1
Vinodhiny <vinodhinybtech@gmail.com>
2024-07-12 17:56:38 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Loginsignup from './Components/LoginSignup/LoginSignup';
+
+
+function App() {
+  return (
+    <div>
+      <Loginsignup/>
+            
+    </div>
+  );
+}
+
+export default App;
